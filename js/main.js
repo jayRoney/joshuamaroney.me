481960aa69c9ddd1f94996022fd3598d27a88e0e
Joshua <maroney4@mail.fresnostate.edu>
2015-01-22 17:13:21 -0800

    adding initial files

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/**
+ * Created by joshuamaroney on 1/12/15.
+ */
+var jumboHeight = $('.jumbotron').outerHeight();
+function parallax(){
+    var scrolled = $(window).scrollTop();
+    $('.bg').css('height', (jumboHeight-scrolled) + 'px');
+}
+
+$(window).scroll(function(e){
+    parallax();
+});
